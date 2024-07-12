6701b04a76ca85e251fe19002121c9f64c959073
Karan-0G <mekarandeepsingh@gmail.com>
2024-07-12 16:46:07 +0530

    signin

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -15,8 +15,8 @@ export const Signin = () => {
     return <div className="bg-slate-300 h-screen flex justify-center">
     <div className="flex flex-col justify-center">
       <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
-        <Heading label={"Sign up"} />
-        <SubHeading label={"Enter your infromation to create an account"} />
+        <Heading label={"Sign in"} />
+        <SubHeading label={"Enter your credentials"} />
         <InputBox onChange={e => {
           setUsername(e.target.value);
         }} placeholder="example@gmail.com" label={"Email"} />
